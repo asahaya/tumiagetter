f3ed0393a4af29b80fda74569fd708852bb53899
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-04-14 12:37:06 +0900

    プロジェクトの初期化

---
+++ lib/src/tumiagetter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Tumiagetter extends StatelessWidget {
+  const Tumiagetter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('つみあげったー'),
+        ),
+      ),
+    );
+  }
+}
