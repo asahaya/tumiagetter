6f95f11a5dc1dbc2f84de23530066d150317ea2b
kawado <kawadon780@gmail.com>
2022-04-16 01:28:16 +0900

    かわど_初期設定とプルリク練習

--- lib/src/tumiagetter.dart
+++ lib/src/tumiagetter.dart
@@ -8,7 +8,7 @@ class Tumiagetter extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('aoiです'),
+          child: Text('かわどです'),
         ),
       ),
     );
