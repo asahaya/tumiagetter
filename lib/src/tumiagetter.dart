5b9ca1ad1b0f570340c0703004986982bf63df2e
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-04-17 17:09:10 +0900

    Issue #4: Initial Setup

--- lib/src/tumiagetter.dart
+++ lib/src/tumiagetter.dart
@@ -8,7 +8,7 @@ class Tumiagetter extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('アサノです'),
+          child: Text('ゆーと'),
         ),
       ),
     );
