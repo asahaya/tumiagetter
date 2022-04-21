acb9ad64af6086b873d6f8ad0d0e879f10114217
asahaya <asa7kadensony@gmail.com>
2022-04-21 22:29:50 +0900

    'Green選択画面再度'

--- lib/select_green_page.dart
+++ lib/select_green_page.dart
@@ -9,13 +9,13 @@ class SelectGreenPage extends StatefulWidget {
 
 class _SelectGreenPageState extends State<SelectGreenPage> {
   Color imageColor = Color(0xFF32CD32);
-
   Color textColor = Color(0xFFFFFFFF);
 
-  final textToDo = ['111', '222', '333'];
-
-  bool onCheck=false;
-
+  final List<ListStatus> _list = <ListStatus>[
+    ListStatus(textData: '筋トレ', isCheck: false),
+    ListStatus(textData: '読書', isCheck: false),
+    ListStatus(textData: 'HTML勉強', isCheck: false),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +37,29 @@ class _SelectGreenPageState extends State<SelectGreenPage> {
       body: Column(
         children: [
           Expanded(
-            flex: 9,
-            child: ListView.builder(
-              itemCount: textToDo.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration:
-                  BoxDecoration(border: Border.all(color: Colors.black)),
-                  child: CheckboxListTile(
-                    title: Text(textToDo[index]),
-                      onChanged:(bool? value){
-                        setState(() {
-                          onCheck=value!;
-                        });
-                      },
-                      value: onCheck,
-                  ),
-                );
-              },
-            ),
-          ),
+              flex: 9,
+              child: ListView(
+                  children: _list
+                      .map(
+                        (ListStatus items) => Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Color(0xFF000000),
+                          )),
+                          child: CheckboxListTile(
+                              title: Text(items.textData!),
+                              value: items.isCheck,
+                              onChanged: (bool? checkValue) {
+                                setState(() {
+                                  items.isCheck = checkValue;
+                                });
+                              }),
+                        ),
+                      )
+                      .toList())),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(15.0),
               child: SizedBox(
                 width: double.maxFinite,
                 child: ElevatedButton(
@@ -76,4 +76,11 @@ class _SelectGreenPageState extends State<SelectGreenPage> {
       ),
     );
   }
+}
+
+class ListStatus {
+  String? textData;
+  bool? isCheck;
+
+  ListStatus({this.textData, this.isCheck});
 }
